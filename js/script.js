719f04cb9249bd90ec30c20b24017a014f4ef570
rommelalansanchez <rommel.alan.sanchez@gmail.com>
2024-10-21 12:09:56 -0700

    Agrega forma de reservacion

--- js/script.js
+++ js/script.js
@@ -27,3 +27,74 @@ function userScroll() {
 }
 
 document.addEventListener("DOMContentLoaded", userScroll);
+
+const reservar = () => {
+  const fecha = document.getElementById("fecha_evento").value;
+  const nombre = document.getElementById("nombre_interesado").value;
+  const correo = document.getElementById("correo_interesado").value;
+  const telefono_interesado = document.getElementById(
+    "telefono_interesado"
+  ).value;
+  const tipoEvento = document.getElementById("tipo_evento").value;
+  const espacio = document.getElementById("espacio_salon").value;
+  const personas = document.getElementById("numero_personas").value;
+  const horario = document.getElementById("horario_evento").value;
+
+  // Your JSON data
+  const jsonData = {
+    Nombre: nombre,
+    Correo: correo,
+    Telefono: telefono_interesado,
+    FechaEvento: fecha,
+    NumeroPersonas: personas,
+    EventoTipo: tipoEvento,
+    Horario: horario,
+  };
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(jsonData),
+  };
+
+  document.getElementById("btnReservar").disabled = true;
+
+  fetch("http://localhost:7093/api/Reservar", options)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Ocurrió un error al reservar.");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      mostrarMensajeReserva();
+      document.getElementById("btnReservar").disabled = false;
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
+    });
+};
+
+let formRes = document.getElementById("form_reservacion");
+formRes.addEventListener("submit", (e) => {
+  e.preventDefault();
+  ocultarMensajeReserva();
+
+  reservar();
+});
+
+const ocultarMensajeReserva = () => {
+  const msg = document.getElementById("MensajeReserva");
+  if (!msg.classList.contains("d-none")) {
+    msg.classList.add("d-none");
+  }
+};
+
+const mostrarMensajeReserva = () => {
+  const msg = document.getElementById("MensajeReserva");
+  if (msg.classList.contains("d-none")) {
+    msg.classList.remove("d-none");
+  }
+};
